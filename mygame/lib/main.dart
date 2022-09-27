e031b482ab4df93e0385eea7fbb73214066c0af2
AldairBalety <aldair.gonzalez@balety.com.mx>
2022-09-27 13:25:09 -0500

    commit 09/27/2022

--- mygame/lib/main.dart
+++ mygame/lib/main.dart
@@ -8,11 +8,34 @@ void main() async {
 }
 
 class MyGame extends FlameGame {
-  SpriteComponent avatar = SpriteComponent();
-  SpriteComponent fondo = SpriteComponent();
+  final DinoPlayer _dinoPlayer = DinoPlayer();
+  final DinoWorld _dinoWorld = DinoWorld();
   @override
   Future<void> onLoad() async {
-    //load game assets
     super.onLoad();
+    await add(_dinoWorld);
+    await add(_dinoPlayer);
+    _dinoPlayer.position = _dinoWorld.size / 1.5;
+    camera.followComponent(_dinoPlayer,
+        worldBounds: Rect.fromLTRB(0, 0, _dinoWorld.size.x, _dinoWorld.size.y));
+  }
+}
+
+class DinoPlayer extends SpriteComponent with HasGameRef {
+  DinoPlayer() : super(size: Vector2.all(100.0));
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    sprite = await gameRef.loadSprite('Avatar.png');
+    position = gameRef.size / 2;
+  }
+}
+
+class DinoWorld extends SpriteComponent with HasGameRef {
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    sprite = await gameRef.loadSprite('Background.png');
+    size = sprite!.originalSize;
   }
 }
